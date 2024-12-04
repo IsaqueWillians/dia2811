4ca361dee3ea46354aae49e9680d1fa87fa02946
Isaque <isaquewillians990@gmail.com>
2024-12-04 20:31:10 -0300

    Exercicos de FI

---
+++ js/exercicio.js
@@ -0,0 +1,8 @@
+var numero1 = parseFloat(prompt("Digite o primeiro numero"));
+var numero2 = parseFloat(prompt("Digite o segundo numero"));
+
+if(numero1 > numero2){
+    alert("o numero 1 é mairo que o numero dois");
+}else{
+    alert("o numeor 2 é maior que o numero 1");
+}
